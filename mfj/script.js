e79528267e8e44c9eeb98e381d492b648d22acd3
kirnatia <natiakiria@gmail.com>
2023-03-21 12:59:31 -0400

    I have created new html, css and js files.

---
+++ mfj/script.js
@@ -0,0 +1,14 @@
+console.log("Hello my dear!");
+var message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+var script1 = 568;
+var script2 = 895;
+var script3 = 214;
+
+var result1, result2, result3;
+result1 = script1 - script3;
+result2 = script2 * script1;
+result3 = script3 + script2;
+
+console.log("Result 1: " + result1);
+console.log("Result 2: " + result2);
+console.log("Result 3: " + result3);
